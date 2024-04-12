dbc1a46da36adfdb370dfbd511c08a3ba1fbfac6
Alysse Palmer <apalmer2583@conestogac.on.ca>
2024-04-12 18:06:06 -0400

    Developed a few functions to make a function PLULookup

--- PLULearningTool/main.cpp
+++ PLULearningTool/main.cpp
@@ -1,12 +1,16 @@
 #pragma warning (disable: 4996)
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <conio.h>
+
+# define SIZE 20
 
 typedef struct Item {
     char name[100];
     int PLU;
     double price;
+    struct Item* NextItemValuePair;
 } Item;
 
 typedef struct CartItem {
@@ -14,11 +18,73 @@ typedef struct CartItem {
     double weight;
 } CartItem;
 
+typedef struct PLUTable {
+    Item* table[SIZE];
+};
+
 //used by the results screen to show mistakes
 typedef struct Result {
     Item* itemPTR;
     int incorrectPLU;
 } Result;
+
+//==== = Database.txt==== =
+//
+//Text file listing all Item data
+//
+//
+//
+//-Format -
+//
+//<Item Name(string)> | <PLU Code(int)> | <Price(double)>
+
+int generateHash(char* itemName, int PLU) {
+    int hash = 0;
+    for (int i = 0; itemName[i] != '\0'; i++) {
+        int asciiValue = itemName[i];
+        hash = (hash + asciiValue + PLU) % SIZE;
+    }
+}
+
+Item* initializeItem(char* nameofItem, int PLUOfItem, double priceOfItem) {
+    Item* newItem = (Item*)malloc(sizeof(Item));
+    if (newItem == NULL) {
+        printf("Not enough memory!");
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy(newItem->name, nameofItem);
+    newItem->PLU = PLUOfItem;
+    newItem->price = priceOfItem;
+    newItem->NextItemValuePair = NULL;
+}
+
+PLUTable* initializePLUTable(void) {
+    PLUTable* table = (PLUTable*)malloc(sizeof(PLUTable));
+    if (table == NULL) {
+        printf("Not enough memory!");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < SIZE; i++) {
+        table->table[i] = NULL;
+    }
+}
+
+void insertItemToTable(PLUTable* table, char* nameOfItem, int PLUOfItem, double costOfItem) {
+    int hash = generateHash(nameOfItem, PLUOfItem);
+
+    Item* newItem = initializeItem(nameOfItem, PLUOfItem, costOfItem);
+
+    if (table->table[hash] == NULL) {
+        table->table[hash] = newItem;
+        return;
+    }
+
+    Item* current = table->table[hash]
+
+}
+
 
 
 
