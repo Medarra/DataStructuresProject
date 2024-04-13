b3ed643fd501cd0347620445655a96195860413a
Curtis Wentzlaff <curtiswentzlaff@gmail.com>
2024-04-12 20:59:47 -0400

    Finished 'makeChange' function.

--- PLULearningTool/main.cpp
+++ PLULearningTool/main.cpp
@@ -18,16 +18,18 @@ typedef struct CartItem {
     double weight;
 } CartItem;
 
-typedef struct PLUTable {
-    Item* table[SIZE];
-} PLUTable;
-
 //used by the results screen to show mistakes
 typedef struct Result {
     Item* itemPTR;
     int incorrectPLU;
 } Result;
 
+
+/*-----------------------------------------HASH TABLE--------------------------------------------*/
+
+typedef struct PLUTable {
+    Item* table[SIZE];
+} PLUTable;
 
 int generateHash(char* itemName) {
     int hash = 0;
@@ -121,25 +123,6 @@ void printPLUSheet(PLUTable* table) {
 
 
 
-
-//==== = HASH TABLE==== =
-//
-//PLU lookup table filled with Items
-//
-//- 20 total item slots
-//
-//- Hash Function : (name(total ASCII value) + price) % 20 == NOT FINAL ==
-//
-//-implement separate chaining
-//
-//- items add be added through the program(add this at the end if we have time)
-//
-//- this will also require writing to the Items file
-
-
-
-
-
 //==== = STACK==== =
 //
 //A cart filled with CartItems
@@ -156,11 +139,12 @@ void printPLUSheet(PLUTable* table) {
 //
 //- holds up to 5 items
 
-
+int makeChange(double, double);
 
 int main(void)
 {
     char choice = '\0';
+    PLUTable* lookupTable = initializePLUTable();
 
     do
     {
@@ -175,6 +159,7 @@ int main(void)
 
         switch (choice = getch()) {
         case '1': /*--Create a Cart for testing--*/
+            makeChange(25,43.40);
             break;
         case '2': /*--Play test session--*/
             break;
@@ -183,27 +168,54 @@ int main(void)
         }
     } while (choice != '4');
 
+    // Free resources 
+    free(lookupTable);
     return 0;
 }
 
-int makeChange(double cost, double asdf) {
-    int userChoice = 0;
-    const char* changeTypes[10] = { "Nickels", "Dimes", "Quarters", "Loonies", "Twoonies", "5's", "10's" };
+int makeChange(double cost, double received) {
+    char userChoice = '\0';
+
+    // Setup for change calculation
+    const char* changeTypes[10] = { "Nickels", "Dimes", "Quarters", "Loonies", "Toonies", "5's", "10's" };
+    double changeAmount[7] = { 0.05, 0.10, 0.25, 1.00, 2.00, 5.00, 10.00 };
     int changeCounter[7] = { 0 };
 
     do {
+        // Displays instructions to the user
         system("CLS");
-        printf("");
+        printf("Total Cart Cost: $%.2lf\nMoney Received from Customer: $%.2lf\n\n", cost, received);
+        printf("Use the numberpad to select change.\nPress <ENTER> to finish and 'r' to reset.\n\n");
         for (int i = 0; i < 7; i++) {
-            printf("Number of %s:\t%d", changeTypes[i], changeCounter[i]);
+            printf("%d. Number of %s:\t%10d\n", (i + 1), changeTypes[i], changeCounter[i]);
         }
 
-        userChoice = getch() - '0';
-        if (userChoice > 0 && userChoice < 8) {
-            changeCounter[userChoice - 1]++;
+        // User input
+        userChoice = getch();
+        if (userChoice > '0' && userChoice < '8') {
+            changeCounter[userChoice - '0' - 1]++;
+        }
+        else if (userChoice == 'r') {
+            for (int i = 0; i < 7; i++) {
+                changeCounter[i] = 0;
+            }
         }
     } while (userChoice != '\r');
 
-    // calculate change here
+    // Calculates total change given
     double change = 0;
+    for (int i = 0; i < 7; i++) {
+        change += (changeAmount[i] * changeCounter[i]);
+    }
+
+    // Print and return result
+    printf("\nChange Required: $%.2lf\n", received - cost);
+    printf("Change Given: $%.2lf\n", change);
+    
+    if (change == (received - cost)) {
+        return 0;
+    }
+    else {
+        return 1;
+    }
 }
