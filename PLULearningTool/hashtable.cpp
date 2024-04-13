e700cf37b18b99053773114bec2701bd2ab0c14c
Curtis Wentzlaff <curtiswentzlaff@gmail.com>
2024-04-13 11:09:00 -0400

    Created hashtable.cpp file for all hashatable structs and functions.

---
+++ PLULearningTool/hashtable.cpp
@@ -0,0 +1,109 @@
+#pragma warning (disable: 4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+
+#define SIZE 20
+#define NAME_LENGTH 50
+
+typedef struct Item {
+    char name[NAME_LENGTH];
+    int PLU;
+    double price;
+    struct Item* NextItemValuePair;
+} Item;
+
+typedef struct PLUTable {
+    Item* table[SIZE];
+} PLUTable;
+
+int generateHash(char* itemName) {
+    int hash = 0;
+    for (int i = 0; itemName[i] != '\0'; i++) {
+        int asciiValue = itemName[i];
+        hash = (hash + asciiValue) % SIZE;
+    }
+    return hash;
+}
+
+Item* initializeItem(char* nameofItem, int PLUOfItem, double priceOfItem) {
+    Item* newItem = (Item*)malloc(sizeof(Item));
+    if (newItem == NULL) {
+        printf("Not enough memory!");
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy(newItem->name, nameofItem);
+    newItem->PLU = PLUOfItem;
+    newItem->price = priceOfItem;
+    newItem->NextItemValuePair = NULL;
+
+    return newItem;
+}
+
+PLUTable* initializePLUTable(void) {
+    PLUTable* table = (PLUTable*)malloc(sizeof(PLUTable));
+    if (table == NULL) {
+        printf("Not enough memory!");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < SIZE; i++) {
+        table->table[i] = NULL;
+    }
+
+    return table;
+}
+
+void insertItemToTable(PLUTable* table, char* nameOfItem, int PLUOfItem, double costOfItem) {
+    int hash = generateHash(nameOfItem);
+
+    Item* newItem = initializeItem(nameOfItem, PLUOfItem, costOfItem);
+
+    if (table->table[hash] == NULL) {
+        table->table[hash] = newItem;
+        return;
+    }
+
+    Item* current = table->table[hash];
+    while (current->NextItemValuePair != NULL) {
+        current = current->NextItemValuePair;
+    }
+
+    current->NextItemValuePair = newItem;
+}
+
+Item* searchForItemInformation(PLUTable* table, char* itemName) {
+    int hash = generateHash(itemName);
+
+    if (table->table[hash] == NULL) {
+        printf("ERROR: Cannot find the word!");
+        return NULL;
+    }
+
+    Item* current = table->table[hash];
+
+    while (current != NULL) {
+        if (strcmp(current->name, itemName) == 0) {
+            return current;
+        }
+
+        current = current->NextItemValuePair;
+    }
+
+    return NULL;
+}
+
+void printPLUSheet(PLUTable* table) {
+    system("CLS");
+    printf("PLU Cheat Sheet\n");
+    printf("===================\n");
+    for (int i = 0; i < SIZE; i++) {
+        Item* current = table->table[i];
+        while (current != NULL) {
+            printf("%s ........................ %d\n", current->name, current->PLU);
+            current = current->NextItemValuePair;
+        }
+    }
+}
--- PLULearningTool/main.cpp
+++ PLULearningTool/main.cpp
@@ -4,17 +4,11 @@
 #include <string.h>
 #include <conio.h>
 
-# define SIZE 20
+#include "hashtable.cpp"
+
 #define NAME_LENGTH 50
 #define CHANGE_NUMBER 7         // Represents the number of different coins/bills that can be given
                                 // as change to the customer (dimes, quarters, etc.)
-
-typedef struct Item {
-    char name[NAME_LENGTH];
-    int PLU;
-    double price;
-    struct Item* NextItemValuePair;
-} Item;
 
 typedef struct CartItem {
     char name[NAME_LENGTH];
@@ -26,103 +20,6 @@ typedef struct Result {
     Item* itemPTR;
     int incorrectPLU;
 } Result;
-
-
-/*-----------------------------------------HASH TABLE--------------------------------------------*/
-
-typedef struct PLUTable {
-    Item* table[SIZE];
-} PLUTable;
-
-int generateHash(char* itemName) {
-    int hash = 0;
-    for (int i = 0; itemName[i] != '\0'; i++) {
-        int asciiValue = itemName[i];
-        hash = (hash + asciiValue)  % SIZE;
-    }
-    return hash;
-}
-
-Item* initializeItem(char* nameofItem, int PLUOfItem, double priceOfItem) {
-    Item* newItem = (Item*)malloc(sizeof(Item));
-    if (newItem == NULL) {
-        printf("Not enough memory!");
-        exit(EXIT_FAILURE);
-    }
-
-    strcpy(newItem->name, nameofItem);
-    newItem->PLU = PLUOfItem;
-    newItem->price = priceOfItem;
-    newItem->NextItemValuePair = NULL;
-
-    return newItem;
-}
-
-PLUTable* initializePLUTable(void) {
-    PLUTable* table = (PLUTable*)malloc(sizeof(PLUTable));
-    if (table == NULL) {
-        printf("Not enough memory!");
-        exit(EXIT_FAILURE);
-    }
-
-    for (int i = 0; i < SIZE; i++) {
-        table->table[i] = NULL;
-    }
-
-    return table;
-}
-
-void insertItemToTable(PLUTable* table, char* nameOfItem, int PLUOfItem, double costOfItem) {
-    int hash = generateHash(nameOfItem);
-
-    Item* newItem = initializeItem(nameOfItem, PLUOfItem, costOfItem);
-
-    if (table->table[hash] == NULL) {
-        table->table[hash] = newItem;
-        return;
-    }
-
-    Item* current = table->table[hash];
-    while (current->NextItemValuePair != NULL) {
-        current = current->NextItemValuePair;
-    }
-
-    current->NextItemValuePair = newItem;
-}
-
-Item* searchForItemInformation(PLUTable* table, char* itemName) {
-    int hash = generateHash(itemName);
-
-    if (table->table[hash] == NULL) {
-        printf("ERROR: Cannot find the word!");
-        return NULL;
-    }
-
-    Item* current = table->table[hash];
-
-    while (current != NULL) {
-        if (strcmp(current->name, itemName) == 0) {
-            return current;
-        }
-
-        current = current->NextItemValuePair;
-    }
-
-    return NULL;
-}
-
-void printPLUSheet(PLUTable* table) {
-    system("CLS");
-    printf("PLU Cheat Sheet\n");
-    printf("===================\n");
-    for (int i = 0; i < SIZE; i++) {
-        Item* current = table->table[i];
-        while (current != NULL) {
-            printf("%s ........................ %d\n", current->name, current->PLU);
-            current = current->NextItemValuePair;
-        }
-    }
-}
 
 
 
@@ -165,12 +62,7 @@ int main(void)
 
         switch (choice = getch()) {
         case '1': /*--Create a Cart for testing--*/
-            if (makeChange(25, 47.20) == 0) {
-                printf("good\n");
-            }
-            else {
-                printf("bad\n");
-            }
+            printf(makeChange(25, 47.20) == 0 ? "good\n" : "bad\n");
             getch();
             break;
         case '2': /*--Play test session--*/
