ac0747ffe4f6b1b77641182320b88abf2b1833dc
wgbnlee <wgbnlee@gmail.com>
2024-04-15 18:59:21 -0400

    edited fillCart function

--- PLULearningTool/main.cpp
+++ PLULearningTool/main.cpp
@@ -28,11 +28,134 @@ typedef struct Result {
 //A cart filled with CartItems
 //
 //- contains exactly 10 items
+Cart* initializeCart(void) {
+	Cart* cart = (Cart*)malloc(sizeof(Cart));
+
+	if (cart == NULL) {                 //error checking 
+		printf("Not enough memory!");
+		exit(EXIT_FAILURE);
+	}
+
+	cart->data = (CartItem*)malloc(MAX_ITEM * sizeof(CartItem));
+
+	if (cart->data == NULL) {           //error checking
+		printf("Not enough memory!");
+		exit(EXIT_FAILURE);
+	}
+
+	cart->topIndex = -1;
+
+	return cart;
+}
+
+//maybe add push part
+Cart* fillCart(PLUTable* PLUtable) {
+	if (PLUtable == NULL) {             //error checking
+		printf("Table is empty!!!");
+		return NULL;
+	}
+
+	Cart* cart = initializeCart();
+
+	int itemCount = countItemInTable(PLUtable);
+
+	if (itemCount <= 0) {               //error checking
+		printf("Table is empty!!!");
+		return NULL;
+	}
+
+	srand(time(NULL));
+	int chosenPLU[MAX_ITEM] = { 0 };
+
+	while (isStackFull(cart) == false) {    //loop until cart is full
+		bool alreadyChosen = false;
+		int randomIndex = 0;
+
+		do {
+			randomIndex = rand() % itemCount;   //random seed
+
+			for (int j = 0; j < cart->topIndex; j++) {       //loop to ensure index has not been chosen yet.
+				//add another data structure for efficiency?
+				if (chosenPLU[j] == randomIndex) {
+					alreadyChosen = true;
+				}
+
+			}
+		} while (alreadyChosen);
+
+		strcpy(cart->data[cart->topIndex].name, PLUtable->table[randomIndex]->name);
+		cart->topIndex++;
+
+		if (cart->topIndex == 0) {
+			continue;                       //to skip assigning value of nextItem.
+		}
+
+		cart->data[cart->topIndex--].nextItem = cart->data;
+	}
+
+	return cart;
+	//fill up cart from hash table
+}
+
+bool isStackFull(Cart* cart) {
+	return cart->topIndex == MAX_ITEM - 1;
+}
+
+int countItemInTable(PLUTable* PLUtable) {
+	Item* current = PLUtable->table[0];
+
+	if (current == NULL) {			//error check
+		printf("No item in the table!!!");
+		return 0;
+	}
+
+	int counter = 1;			//initialized to 1 because of loop structure
+
+	while (current->NextItemValuePair != NULL) {	//loop to count until no next element
+		current = current->NextItemValuePair;
+		counter++;
+	}
+
+	return counter;
+}
+
+ConveyorBelt* initializingConveyorBelt(void) {
+	ConveyorBelt* belt = (ConveyorBelt*)malloc(sizeof(ConveyorBelt));
 
 
+	if (belt == NULL) {
+		printf("Not enough memory!");
+		exit(EXIT_FAILURE);
+	}
 
 
+	belt->front = NULL;
+	belt->back = NULL;
 
+	return belt;
+}
+
+ConveyorBelt* fillingConveyorBelt(Cart* cart) {
+	ConveyorBelt* belt = initializingConveyorBelt();
+
+	for (int i = 0; i < MAX_CONVEYOR_BELT - 1; i++) {               //adds 5 items to belt
+		CartItem* newItem = (CartItem*)malloc(sizeof(CartItem));
+		*newItem = cart->data[i];
+
+		if (belt->front == NULL) {
+			belt->front = newItem;
+			belt->back = newItem;
+
+			//need popping stack
+		}
+
+		belt->back->nextItem = newItem;
+		belt->back = newItem;
+		//need popping stack
+	}
+
+	return belt;
+}
 //==== = QUEUE==== =
 //
 //Cashier conveyor filled with CartItems
